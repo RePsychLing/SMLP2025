d51e434fac3e62dff9a44f0714cbdbb6120b9362
Phillip Alday <me@phillipalday.com>
2025-08-26 15:19:02 +0200

    moar

--- sesssions/2025-08-26-tuesday-pm.jl
+++ sesssions/2025-08-26-tuesday-pm.jl
@@ -1,8 +1,14 @@
 using CairoMakie
 using MixedModels
 using MixedModelsMakie
-using SMLP2025: datasets
+using SMLP2025: dataset
 
 kb07 = dataset("kb07")
+contrasts = Dict(:spkr => HelmertCoding(),
+                 :prec => HelmertCoding(),
+                 :load => HelmertCoding())
 
-fm_max = lmm(@formula(rt_))
+fm_max = lmm(@formula(rt_trunc ~ 1 + spkr * load * prec +
+                                (1 + spkr * load * prec | subj) +
+                                (1 + spkr * load * prec | item)),
+             kb07; contrasts=contrasts)
