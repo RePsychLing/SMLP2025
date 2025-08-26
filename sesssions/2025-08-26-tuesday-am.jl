e42f404621587234e2f65b2f0910437cac684939
Phillip Alday <me@phillipalday.com>
2025-08-26 09:15:41 +0200

    moar

--- sesssions/2025-08-26-tuesday-am.jl
+++ sesssions/2025-08-26-tuesday-am.jl
@@ -14,7 +14,20 @@ lmm(@formula(reaction ~ 1 + days + (1 + days| subj)),
     progress=progress)
 
 # after the semicolon, you can just write the keyword/named argument
-
+# instead of kwarg=kwarg -- e.g., you can write "progress" instead of "progress=progress"
 lmm(@formula(reaction ~ 1 + days + (1 + days| subj)), 
     dataset(:sleepstudy);
-    progress)    
+    progress)
+
+# doesn't work -- positional arguments have a particular position :D
+lmm(dataset(:sleepstudy), @formula(reaction ~ 1 + days + (1 + days| subj)))
+    
+# julia distinguishes between characters (single quotes) and strings (double quotes)
+# you'll almost always want double quotes
+
+# one other weird thing about strings
+# string concatenation is with *
+"a" * "b"
+
+# as a corollary, string repetition is done with exponentiation, i.e. ^
+"ab"^3
