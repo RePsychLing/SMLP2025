707055b4c7a70855b4cd33bf1b7315d03199308e
Phillip Alday <me@phillipalday.com>
2025-08-26 15:55:18 +0200

    wip

--- sesssions/2025-08-26-tuesday-pm.jl
+++ sesssions/2025-08-26-tuesday-pm.jl
@@ -8,6 +8,10 @@ fm_sleep = lmm(@formula(reaction ~ 1 + days + (1+days|subj)),
 
 shrinkageplot(fm_sleep)
 
+shrinkageplot!(Figure(;size=(600,600)), fm_sleep; ellipse=true)
+
+caterpillar(fm_sleep)
+
 kb07 = dataset("kb07")
 contrasts = Dict(:spkr => HelmertCoding(),
                  :prec => HelmertCoding(),
@@ -17,3 +21,9 @@ fm_max = lmm(@formula(rt_trunc ~ 1 + spkr * load * prec +
                                 (1 + spkr * load * prec | subj) +
                                 (1 + spkr * load * prec | item)),
              kb07; contrasts=contrasts)
+
+shrinkageplot(fm_max, :subj)
+
+fm_max.rePCA[:subj]
+
+fm_max.PCA[:subj]
