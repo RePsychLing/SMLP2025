44396e6a1a640fabff984731a0351718dcf64055
Phillip Alday <me@phillipalday.com>
2025-08-28 13:42:25 +0200

    restoreoptsum!

--- sesssions/2025-08-28-thursday-pm.jl
+++ sesssions/2025-08-28-thursday-pm.jl
@@ -7,3 +7,13 @@ fm1 = lmm(@formula(rt_trunc ~ spkr * prec * load +
                               (1 + spkr * prec * load|item) +
                               (1 + spkr * prec * load|subj)),
           dataset(:kb07))
+
+saveoptsum("big_mod.json", fm1)
+
+# just construct, don't fit
+reloaded = LinearMixedModel(@formula(rt_trunc ~ spkr * prec * load +
+                              (1 + spkr * prec * load|item) +
+                              (1 + spkr * prec * load|subj)),
+          dataset(:kb07))
+
+restoreoptsum!(reloaded, "big_mod.json")
