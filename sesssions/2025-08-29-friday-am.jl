76b58b628f7d82bfe1ddbb4a6af2955e0935cf82
Phillip Alday <me@phillipalday.com>
2025-08-29 09:48:53 +0200

    wip

--- sesssions/2025-08-29-friday-am.jl
+++ sesssions/2025-08-29-friday-am.jl
@@ -1,19 +1,15 @@
 using MixedModels, MixedModelsExtras, DataFrames
-
 using MixedModelsDatasets: dataset
 using GLM
 
-fm0 = lm(@formula(reaction ~ 1 + days), dataset(:sleepstudy))
-
-
-fm1 = lmm(@formula(reaction ~ 1 + days + (1 | subj)), dataset(:sleepstudy))
-
-
-
+# fm0 = lm(@formula(reaction ~ 1 + days), dataset(:sleepstudy))
+# fm1 = lmm(@formula(reaction ~ 1 + days + (1 | subj)), dataset(:sleepstudy))
 
 a = lmm(@formula(reaction ~ 1 + days + (1 | subj)), dataset(:sleepstudy))
 b = lmm(@formula(reaction ~ 1 + days + (1 + days | subj)), dataset(:sleepstudy))
 c = lmm(@formula(reaction ~ 1 + (1 | subj)), dataset(:sleepstudy))
 d = lmm(@formula(reaction ~ 1 + (1 + days | subj)), dataset(:sleepstudy))
 
-# tbl = ictable(a, b, c, d)
+tbl = DataFrame(ictable(a, b, c, d))
+tbl = DataFrame(ictable(a, b, c, d; label=["x", "y", "turkey", "duck"]))
+transform(tbl, Not(:model) .=> ByRow(x -> round(Int, x)); renamecols=false)
