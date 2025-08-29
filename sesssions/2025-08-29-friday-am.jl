5d8fec309a2d4115ff7271f0fb2596251f386968
Phillip Alday <me@phillipalday.com>
2025-08-29 09:37:43 +0200

    wip

--- sesssions/2025-08-29-friday-am.jl
+++ sesssions/2025-08-29-friday-am.jl
@@ -1,6 +1,15 @@
 using MixedModels, MixedModelsExtras, DataFrames
 
 using MixedModelsDatasets: dataset
+using GLM
+
+fm0 = lm(@formula(reaction ~ 1 + days), dataset(:sleepstudy))
+
+
+fm1 = lmm(@formula(reaction ~ 1 + days + (1 | subj)), dataset(:sleepstudy))
+
+
+
 
 a = lmm(@formula(reaction ~ 1 + days + (1 | subj)), dataset(:sleepstudy))
 b = lmm(@formula(reaction ~ 1 + days + (1 + days | subj)), dataset(:sleepstudy))
